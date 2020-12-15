7b1fa2dd4aa6e7aaa6a0b4df91d848fda574cb44
sdacosta13 <64925090+sdacosta13@users.noreply.github.com>
2020-12-15 17:35:31 +0000

    Working on key presses

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Red;
+            this.player.Location = new System.Drawing.Point(960, 540);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(50, 50);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox player;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,79 @@ namespace _2dGame
 {
     public partial class Form1 : Form
     {
+        bool movingLeft = false, movingRight = false, movingUp = false, movingDown = false;
         public Form1()
         {
             InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Threading.Thread t = new System.Threading.Thread(DoThisAllTheTime);
+            t.Start();
+        }
+        private void keyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.W)
+            {
+                movingUp = true;
+            }
+            else if(e.KeyCode == Keys.S)
+            {
+                movingDown = true;
+            }
+            else if(e.KeyCode == Keys.A)
+            {
+                movingLeft = true;
+            }
+            else if(e.KeyCode == Keys.D)
+            {
+                movingRight = true;
+            }
+        }
+
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                movingUp = false;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                movingDown = false;
+            }
+            else if (e.KeyCode == Keys.A)
+            {
+                movingLeft = false;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                movingRight = false;
+            }
+        }
+        public void DoThisAllTheTime()
+        {
+            while (true)
+            {
+                int x = 0;
+                int y = 0;
+                if (movingLeft)
+                {
+                    x -= 1;
+                }
+                if(movingRight)
+                {
+                    x += 1;
+                }
+                if (movingUp)
+                {
+                    y -= 1;
+                }
+                if(movingDown)
+                {
+                    y += 1;
+                }
+            }
         }
     }
 }
